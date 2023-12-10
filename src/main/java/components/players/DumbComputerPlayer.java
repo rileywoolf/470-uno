e6af3a1b26a73bcc2906f8cfbccaf435555f0d53
Riley Woolf <89953396+rileywoolf@users.noreply.github.com>
2023-12-09 18:03:50 -0700

    fixed DumbComputerPlayer to call Uno half of the time

--- src/main/java/components/players/DumbComputerPlayer.java
+++ src/main/java/components/players/DumbComputerPlayer.java
@@ -10,16 +10,27 @@ import java.util.Random;
  * This player selects the first valid card in its hand and plays it. Additionally, when required
  * to choose a color (e.g., after playing a Wild card), it randomly selects one of the available colors.
  *
+ * <p>
+ * The class extends the {@link Player} class and implements a straightforward strategy for playing
+ * cards based on the first valid move encountered in its hand. When required to choose a color,
+ * the player randomly selects one of the four available colors.
+ * </p>
+ *
+ * <p>
+ * This class is designed for demonstration and testing purposes, providing a basic AI opponent.
+ * </p>
+ *
  * @author Riley Woolf
  * @version 1.0
  */
 public class DumbComputerPlayer extends Player {
+    private final Random rand = new Random();
 
     /**
-     * Constructs a new DumbComputerPlayer with the default name "Simple Computer Player."
+     * Constructs a new DumbComputerPlayer with the default name "Simple Computer."
      */
     public DumbComputerPlayer() {
-        super("Simple Computer Player");
+        super("Simple Computer");
     }
 
     /**
@@ -51,6 +62,10 @@ public class DumbComputerPlayer extends Player {
             validMove = hand.get(index).validMove(topCard);
         } while (!validMove);
 
+        // Calls UNO when applicable only some of the time.
+        if (hand.size() == 2) {
+            declaredUno = rand.nextBoolean();
+        }
         return hand.remove(index);
     }
 
@@ -62,9 +77,8 @@ public class DumbComputerPlayer extends Player {
      */
     @Override
     public Color chooseColor() {
-        Random random = new Random();
         Color color;
-        switch (random.nextInt(4)) {
+        switch (rand.nextInt(4)) {
             case 0 -> color = Color.BLUE;
             case 1 -> color = Color.GREEN;
             case 2 -> color = Color.RED;
